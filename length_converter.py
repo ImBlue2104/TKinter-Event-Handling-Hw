9410578ff23906f99ec5bbb596efd86d938d55d4
Neel Patil <neel.rp21@gmail.com>
2025-09-02 23:51:39 -0700

    messagebox added and bugs fixed

--- length_converter.py
+++ length_converter.py
@@ -7,14 +7,14 @@ root.title("Length Converter App")
 root.geometry("400x400")
 
 def imp_info():
-    messagebox.showinfo("Read Important" , "This program can only covert Inches nto Centimeteres!")
+    messagebox.showinfo("Read Important" , "This program can only covert Inches into Centimeters!")
 
-Button(root, text = "Read; Important!!!", fg = 'white', bg='black', relief='sunken', command = imp_info).pack()
+Button(root, text = "Read; Important!!!", fg = 'white', bg='red', relief='sunken', command = imp_info).pack()
 #Screen Title
 Label(root, text="Welcome to the Length Conversion App", fg = 'white', bg='black').pack()
 
 #Getting original len fro user
-Label(root, text="Enter Length in Inches to conver to Centimetres:", fg='black', bg='#d0efff').pack()
+Label(root, text="Enter Length in Inches to conver to Centimeters:", fg='black', bg='#d0efff').pack()
 Length= Entry(root, fg="black", bg='grey', width=50)
 Length.pack()
 
@@ -27,9 +27,9 @@ def convert(length):
 
 def display():
         # Get values from entries *when button is clicked*
-    l = Length.get()
-    Message = Label(root, text = f"{l} inches is equal to {convert(l)} centimeteres!")
-    Message.place(x = 130, y = 250)
+    l = int(Length.get())
+    Message = Label(root, text = f"{l} inches is equal to {convert(l)} centimeters!", fg = 'white', bg ='blue')
+    Message.pack()
 Button(root, text = "Submit", fg = 'white', bg='black', relief='sunken', command = display).pack()
 
 root.mainloop()
